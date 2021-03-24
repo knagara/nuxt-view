09507cfc7573646a3504e312a2d5ce0eb3d51cfb
knagara <nagara.keita@gmail.com>
2021-03-24 12:54:38 +0900

    init

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxt-view',
+    title: 'Nuxt View Ssample',
     htmlAttrs: {
       lang: 'en'
     },
@@ -11,7 +11,9 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com'},
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Train+One&display=swap'}
     ]
   },
 
